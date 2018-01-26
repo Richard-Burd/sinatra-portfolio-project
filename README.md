65410548a2443a18f31dec8c6126fee01c9682a5
Richard Burd <richardburdx@yahoo.com>
2018-01-26 17:05:21 +0000

    added remaining folders & the modules

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sinatra Portfolio Project
+
+## Overview
+
+This is a Portfolio Project for Flatiron School.  Stay tuned for more information in this readme file!
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
+require 'capybara/dsl'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
