6a74853f1afbb422ebdef13084ca411ceee2121f
Richard Burd <richardburdx@yahoo.com>
2018-01-29 15:18:54 +0000

    changed email_address to just email for User class

--- db/migrate/20180126172230_create_users.rb
+++ db/migrate/20180126172230_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email_address
+      t.string :email
       t.string :password_digest
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20180128042726) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email_address"
+    t.string "email"
     t.string "password_digest"
   end
 
