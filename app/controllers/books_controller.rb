e56d54b42bdc9d7880919fa2b4dbe82ad47a9c85
Richard Burd <richardburdx@yahoo.com>
2018-01-30 23:04:42 +0000

    got the '/books/new' to display properly but not function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
 
   get '/books/new' do
+    @authors = Author.all # this is required to list out all existing authors
     if logged_in?
       erb :'books/create_book'
     else
@@ -13,7 +14,18 @@ class BooksController < ApplicationController
   end
 
   get '/books/:slugtitle' do
-    @book = Book.find_by_slugtitle(params[:slugtitle])
+    @books = Book.find_by_slugtitle(params[:slugtitle])
     erb :'books/show_book'
+  end
+
+  post '/books/new/book/address/here' do
+    @book = Book.create(params[:title])
+  #  @books = Book.find(params[:slugtitle])
+  #  @books.update(params["book"])
+  #  if !params["author"]["name"].empty?
+  #    @books.author = Author.create(name: params["author"]["name"])
+  #  end
+  #  @books.save
+  #  redirect "books/#{@books.title.slugtitle}"
   end
 end
