20df16081c8766a09eb46c1ac0f51ce01408fd23
Richard Burd <richardburdx@yahoo.com>
2018-01-31 16:55:15 +0000

    can add new book with existing author but nothing else

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,8 @@ class BooksController < ApplicationController
 
   get '/books/new' do
     @authors = Author.all # this is required to list out all existing authors
+    @genres = Author.all # this is required to list out all existing genres
+    @languages = Author.all # this is required to list out all existing languages
     if logged_in?
       erb :'books/create_book'
     else
@@ -20,8 +22,16 @@ class BooksController < ApplicationController
 
   post '/books' do
 #    raise params.inspect
+    @unknown_author = Author.create(name: params[:author][:name], birth_date: params[:author][:birth_date], death_date: params[:author][:death_date]) # I need this in order to assign an "unknown author"
     @book = Book.create(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id])
-    @book.save
-    erb :'books/books'
+    @unknown_author = @book.author
+#    @unknown_author.unknown_author # <= this method renames the "unknown" author to include the new book name that was just created.
+#    @unknown_author.save
+#    @book.save
+#  This method below is in the seed data that does the same thing to all existing authors with an "unknown" or nil name.
+#    Book.all.each do |book|
+#      book.unknown_author
+#    end
+    redirect :'books'
   end
 end
