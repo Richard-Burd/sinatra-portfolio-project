adcf4d0007ffe15cc05597b268b1fa86a02ce665
Richard Burd <richardburdx@yahoo.com>
2018-01-31 23:21:57 +0000

    unknown authors no longer listed on '/book/new' & can now select multiple genres

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,7 +30,12 @@ class BooksController < ApplicationController
 #         {"language.id"=>"4"}
 #     }
 
-    @book = Book.create(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id])
+    @book = Book.new(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id])
+    if @book.year_published == ""
+      @book.year_published = nil #<= so this activates the <<hypothetical_date_of_publication>> methd
+    end
+    @book.unknown_author         #<= when you create a new author named "Unknown" and then you list
+    @book.save
 
     params[:booklanguage][:langs].each do |details|
       BookLanguage.create(details)
@@ -43,8 +48,19 @@ class BooksController < ApplicationController
       end
     end
 
-  #  @booklanguage = BookLanguage.create(book_id: @book.id, language_id: params[:booklanguage]["language.id"])
-    @bookgenre = BookGenre.create(book_id: @book.id, genre_id: params[:bookgenre]["genre.id"])
+#   Ok, so this 'genre' thingy is just the same as the 'language' one above.
+    params[:bookgenre][:gens].each do |details|
+      BookGenre.create(details)
+    end
+
+    BookGenre.all.each do |bookgenre|
+      if bookgenre.book_id == nil
+        bookgenre.book_id = Book.last.id
+        bookgenre.save
+      end
+    end
+  #  @booklanguage = BookLanguage.create(book_id: @book.id, language_id: params[:booklanguage]["language.id"])  <= This only would work if there was one book
+  #  @bookgenre = BookGenre.create(book_id: @book.id, genre_id: params[:bookgenre]["genre.id"])                 <= This only would work if there was one genre
     redirect to :'books'
   end
 end
