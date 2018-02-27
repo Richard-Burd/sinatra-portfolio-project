df3d05cc4b579650f96bceff29687830a44dc4ad
Richard Burd <richardburdx@yahoo.com>
2018-02-27 21:48:14 +0000

    deleted superfluous user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :'users/show'
-  end
 
   get '/signup' do
     if !logged_in?
