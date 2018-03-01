add964702830011a0ef6b4458eb1181918a43a8e
Richard Burd <richardburdx@yahoo.com>
2018-03-01 00:40:08 +0000

    Implemented build method in the book post request

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,46 +4,7 @@ class BooksController < ApplicationController
     erb :'books/books'
   end
 
-  get '/books/new' do # This ie exactly the same as "post '/books/:slugtitle/edit' do" below
-#   raise params.inspect
-#   OK, so basically, we're building a super complex, super nested hash here.
-#   We have to do this in order to use the data from the BookLanguage and BookGenre classes.
-#   This is because that data is all nothing but a bunch of integers, so it will get really
-#   confusing if we don't have a way to sort out the data on the BookController end so we can
-#   take this data from this hash below and then transfer it into Active-Record-Land via the
-#   this BookController class.
-=begin
-      "book"=> {
-        "title"=>"Diwan-e-Kabir",
-        "topics"=>"A collection of lyric poems that contains more than 40,000 verses.",
-        "year_published"=>"",
-        "author_id"=>"8"
-      },
-        "booklanguage"=> {
-          "langs"=>  [
-                       {
-            "language_id"=>"3"
-          },
-                       {
-            "language_id"=>"6"
-          }
-         ]
-        },
-        "bookgenre"=>  {
-          "gens"=> [
-                       {
-            "genre_id"=>"7"
-          },
-          {
-            "genre_id"=>"14"
-          },
-          {
-            "genre_id"=>"10"
-          }
-         ]
-        }
-       }
-=end
+  get '/books/new' do
     @authors = Author.all # this is required to list out all existing authors
     @genres = Genre.all # this is required to list out all existing genres
     @languages = Language.all # this is required to list out all existing languages
@@ -52,6 +13,45 @@ class BooksController < ApplicationController
     else
       flash[:message] ="Oops, sorry but you must login below or sign up here first before creating a new book."
       erb :'users/login'
+    end
+  end
+
+  post '/books' do
+    if logged_in?
+      @book = current_user.books.build(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id])
+      if @book.year_published == ""
+        @book.year_published = nil #<= so this activates the <<hypothetical_date_of_publication>> method so if you don't know when the book was published, it will estimate that for you.
+      end
+      @book.unknown_author         #<= when you create a new author named "Unknown" and then you list
+
+      params[:booklanguage][:langs].each do |details|
+        BookLanguage.create(details)
+      end
+
+      BookLanguage.all.each do |booklanguage|
+        if booklanguage.book_id == nil
+          booklanguage.book_id = Book.last.id
+          booklanguage.save
+        end
+      end
+
+#     Ok, so this 'genre' thingy is just the same as the 'language' one above.
+      params[:bookgenre][:gens].each do |details|
+        BookGenre.create(details)
+      end
+
+      BookGenre.all.each do |bookgenre|
+        if bookgenre.book_id == nil
+          bookgenre.book_id = Book.last.id
+          bookgenre.save
+        end
+      end
+
+      if @book.save
+        redirect to "/books"
+      else
+        redirect to "/books/new"
+      end
     end
   end
 
@@ -164,42 +164,6 @@ class BooksController < ApplicationController
   get '/books/:slugtitle' do
     @book = Book.find_by_slugtitle(params[:slugtitle])
     erb :'books/show_book'
-  end
-
-  post '/books' do # this recieves the post action of "create_book.erb"
-    @book = Book.new(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id])
-    if @book.year_published == ""
-      @book.year_published = nil #<= so this activates the <<hypothetical_date_of_publication>> method so if you don't know when the book was published, it will estimate that for you.
-    end
-    @book.unknown_author         #<= when you create a new author named "Unknown" and then you list
-    @book.user_id = session[:user_id]
-    @book.save
-
-    params[:booklanguage][:langs].each do |details|
-      BookLanguage.create(details)
-    end
-
-    BookLanguage.all.each do |booklanguage|
-      if booklanguage.book_id == nil
-        booklanguage.book_id = Book.last.id
-        booklanguage.save
-      end
-    end
-
-#   Ok, so this 'genre' thingy is just the same as the 'language' one above.
-    params[:bookgenre][:gens].each do |details|
-      BookGenre.create(details)
-    end
-
-    BookGenre.all.each do |bookgenre|
-      if bookgenre.book_id == nil
-        bookgenre.book_id = Book.last.id
-        bookgenre.save
-      end
-    end
-  #  @booklanguage = BookLanguage.create(book_id: @book.id, language_id: params[:booklanguage]["language.id"])  <= This only would work if there was one language
-  #  @bookgenre = BookGenre.create(book_id: @book.id, genre_id: params[:bookgenre]["genre.id"])                 <= This only would work if there was one genre
-    redirect to :'books'
   end
 
   delete '/books/:slugtitle/delete' do
