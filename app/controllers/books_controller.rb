4490947105914f3d85c5f0eb7b78c5bcf8ca809f
Richard Burd <richardburdx@yahoo.com>
2018-03-02 21:40:10 +0000

    fixed if statement with no block

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -85,57 +85,55 @@ class BooksController < ApplicationController
   patch '/books/:slugtitle' do
     if logged_in?
       @book = Book.find_by_slugtitle(params[:slugtitle])
-      if @book.user == current_user
-        if @book.update(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id]) != true
-          flash[:message] = "Sorry, but that book title is already taken, please choose another title."
-          redirect to '/books/new' #<= You can't redirect to the '/books/:slugtitle/edit for some reason...even if you are logged in as the user that created it.'
-        end
-
-        if @book.year_published == ""
-          @book.year_published = nil #<= so this activates the <<hypothetical_date_of_publication>> method so if you don't know when the book was published, it will estimate that for you.
-        end
-        @book.unknown_author         #<= when you create a new author named "Unknown" and then you list
-        @book.save
-
-        # first, you have to delete all BookLanguages with associations of the current book, this is in the "edit" post, but not in the "create" post.
-        BookLanguage.all.each do |booklanguage|
-          if booklanguage.book_id == @book.id
-            booklanguage.delete
-            booklanguage.save
-          end
-        end
-
-        params[:booklanguage][:langs].each do |details|
-          BookLanguage.create(details)
-        end
-
-        BookLanguage.all.each do |booklanguage|
-          if booklanguage.book_id == nil
-            booklanguage.book_id = @book.id
-            booklanguage.save
-          end
-        end
-
-#       Ok, so this 'genre' thingy is just the same as the 'language' one above.
-        BookGenre.all.each do |bookgenre|
-          if bookgenre.book_id == @book.id
-            bookgenre.delete
-            bookgenre.save
-          end
-        end
-
-        params[:bookgenre][:gens].each do |details|
-          BookGenre.create(details)
-        end
-
-        BookGenre.all.each do |bookgenre|
-          if bookgenre.book_id == nil
-            bookgenre.book_id = @book.id
-            bookgenre.save
-          end
-        end
-        erb :'books/show_book'
+      if @book.update(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id]) != true
+        flash[:message] = "Sorry, but that book title is already taken, please choose another title."
+        redirect to '/books/new' #<= You can't redirect to the '/books/:slugtitle/edit for some reason...even if you are logged in as the user that created it.'
       end
+
+      if @book.year_published == ""
+        @book.year_published = nil #<= so this activates the <<hypothetical_date_of_publication>> method so if you don't know when the book was published, it will estimate that for you.
+      end
+      @book.unknown_author         #<= when you create a new author named "Unknown" and then you list
+      @book.save
+
+      # first, you have to delete all BookLanguages with associations of the current book, this is in the "edit" post, but not in the "create" post.
+      BookLanguage.all.each do |booklanguage|
+        if booklanguage.book_id == @book.id
+          booklanguage.delete
+          booklanguage.save
+        end
+      end
+
+      params[:booklanguage][:langs].each do |details|
+        BookLanguage.create(details)
+      end
+
+      BookLanguage.all.each do |booklanguage|
+        if booklanguage.book_id == nil
+          booklanguage.book_id = @book.id
+          booklanguage.save
+        end
+      end
+
+#     Ok, so this 'genre' thingy is just the same as the 'language' one above.
+      BookGenre.all.each do |bookgenre|
+        if bookgenre.book_id == @book.id
+          bookgenre.delete
+          bookgenre.save
+        end
+      end
+
+      params[:bookgenre][:gens].each do |details|
+        BookGenre.create(details)
+      end
+
+      BookGenre.all.each do |bookgenre|
+        if bookgenre.book_id == nil
+          bookgenre.book_id = @book.id
+          bookgenre.save
+        end
+      end
+      erb :'books/show_book'
     end
   end
 
