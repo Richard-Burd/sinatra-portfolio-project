b50606d730c1691a39b969de9fc06ad80e2a079e
Richard Burd <richardburdx@yahoo.com>
2018-03-06 17:29:27 +0000

    books_controller.rb post & patch requests only use single hash to alter database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,25 +20,11 @@ class BooksController < ApplicationController
     if logged_in?
       @book = current_user.books.build(params[:book])
       @book.unknown_author
-
-      if @book.save # This is checking to see if the <<<validates :title, uniqueness: { case_sensitive: false }>> ...is true or not
-        params[:book_languages].each do |language|
-          book_language = BookLanguage.new(language)
-          book_language.book_id = @book.id #<= this can only happen if the <<@book>> saves!
-          book_language.save
-        end
-
-        params[:book_genres].each do |genre|
-          book_genre = BookGenre.new(genre)
-          book_genre.book_id = @book.id #<= this can only happen if the <<@book>> saves!
-          book_genre.save
-        end
-
-        redirect to "/books"
-      else
-        flash[:message_for_new_book_page] = "Sorry, but that book title is already taken, please choose another title."
-        redirect to "/books/new"
-      end
+      @book.save
+      redirect to "/books"
+    else
+      flash[:message_for_new_book_page] = "Sorry, but that book title is already taken, please choose another title."
+      redirect to "/books/new"
     end
   end
 
@@ -67,56 +53,15 @@ class BooksController < ApplicationController
   patch '/books/:slugtitle' do
     if logged_in?
       @book = Book.find_by_slugtitle(params[:slugtitle])
-      if @book.update(title: params[:book][:title], topics: params[:book][:topics], year_published: params[:book][:year_published], author_id: params[:book][:author_id]) != true
-        flash[:message_for_new_book_page] = "Sorry, but that book title is already taken, please choose another title."
-        redirect to '/books/new' #<= You can't redirect to the '/books/:slugtitle/edit for some reason...even if you are logged in as the user that created it.'
-      end
-
-      @book.unknown_author         #<= when you create a new author named "Unknown" and then you list
-
-      if @book && @book.user == current_user # is this the same as: @book.user_id == current_user.id -?
-        @book.save
+      if @book && @book.user == current_user
+        if @book.update(params[:book]) != true
+          flash[:message_for_new_book_page] = "Sorry, but that book title is already taken, please choose another title."
+          redirect to '/books/new'
+        end
+        @book.unknown_author
       else
         flash[:message_for_login_page] = "Sorry, but you don't have permission to edit a book you didn't create."
         erb :'users/login'
-      end
-
-      # first, you have to delete all BookLanguages with associations of the current book, this is in the "edit" post, but not in the "create" post.
-      BookLanguage.all.each do |booklanguage|
-        if booklanguage.book_id == @book.id
-          booklanguage.delete
-          booklanguage.save
-        end
-      end
-
-      params[:booklanguage][:langs].each do |details|
-        BookLanguage.create(details)
-      end
-
-      BookLanguage.all.each do |booklanguage|
-        if booklanguage.book_id == nil
-          booklanguage.book_id = @book.id
-          booklanguage.save
-        end
-      end
-
-#     Ok, so this 'genre' thingy is just the same as the 'language' one above.
-      BookGenre.all.each do |bookgenre|
-        if bookgenre.book_id == @book.id
-          bookgenre.delete
-          bookgenre.save
-        end
-      end
-
-      params[:bookgenre][:gens].each do |details|
-        BookGenre.create(details)
-      end
-
-      BookGenre.all.each do |bookgenre|
-        if bookgenre.book_id == nil
-          bookgenre.book_id = @book.id
-          bookgenre.save
-        end
       end
       erb :'books/show_book'
     end
